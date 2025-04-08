6ecbdbd01cac84ea9023efaa9fe3b1ae912eb653
MADHAV <challa.madhavarao2016@gmail.com>
2025-04-08 12:26:36 +0530

    Update main.tf

--- Terraform-project-S3-creation/Env/Dev/Vpc/main.tf
+++ Terraform-project-S3-creation/Env/Dev/Vpc/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 module "vpc" {
   source               = "../../../modules/Vpc"
   aws_region           = "us-east-1"
-  vpc_cidr             = "10.0.0.0/16"
-  public_subnet_cidr   = "10.0.1.0/24"
-  private_subnet_cidr  = "10.0.2.0/24"
+  vpc_cidr             = "178.35.14.0/16"
+  public_subnet_cidr   = "178.35.14.0/24"
+  private_subnet_cidr  = "178.35.14.0/24"
   availability_zone    = "us-east-1a"
   env                  = "dev"
 }
